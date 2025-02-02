6b1eedc4bf476e074679d2c5a4f40755f5204d95
gredd182 <gyaswanth9347@gmail.com>
2025-02-02 14:12:31 +0530

    Initial commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 
 module "s3_bucket" {
   source  = "app.terraform.io/Private_Registry/s3-bucket/aws"
-  version = "2.1.0"
+  version = "4.1.0"
 
   bucket_name = "prod-bucket"
   acl         = "private"
